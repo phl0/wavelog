31a196c751d985f685abd37fce1e9bca48ddded7
phl0 <github@florian-wolters.de>
2025-11-12 21:36:25 +0100

    More replacements

--- application/views/interface_assets/header.php
+++ application/views/interface_assets/header.php
@@ -402,7 +402,7 @@
 							<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">
 								<?php if ($this->session->userdata('clubstation') == 1) {
 									echo '<i class="fas fa-users"></i> '
-										. '<b>' . str_replace("0","&Oslash;", strtoupper($this->session->userdata('user_callsign'))) . '</b>'
+										. '<b>' . strtoupper($this->session->userdata('user_callsign')) . '</b>'
 										. ' <br><small>'
 										. sprintf(_pgettext("Operator: Callsign", "Op: %s"), str_replace("0","&Oslash;", strtoupper($this->session->userdata('operator_callsign'))))
 										. '</small>';
